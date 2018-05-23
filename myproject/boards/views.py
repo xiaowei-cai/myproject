820fa2ec952ddd9a171bce2c7cccc2803da46905
xiaowei_cai <caixiaowei910325@163.com>
2018-05-23 17:50:09 +0800

    return render

--- myproject/boards/views.py
+++ myproject/boards/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Board
 
 # Create your views here.
 
 
 def home(request):
-    return HttpResponse("Hello World")
+    # return HttpResponse("Hello World")
+    boards = Board.objects.all()
+    # board_names = list()
+    #
+    # for board in boards:
+    #     board_names.append(board.name)
+    #
+    # response_html = '<br>'.join(board_names)
+
+    return render(request, 'home.html', {'boards': boards})
